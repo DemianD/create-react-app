d6674fbf684349bceaa2d79c1d0c80165a390d8b
Demian Dekoninck <demian@live.be>
2019-10-03 20:01:02 +0200

    clean css

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="content-center">
         <header className="min-h-screen flex flex-col items-center justify-center text-2xl">
-          <img
-            src={logo}
-            className="h-48 pointer-events-none App-logo"
-            alt="logo"
-          />
+          <img src={logo} className="h-48 pointer-events-none" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
